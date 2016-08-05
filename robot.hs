575da38783d3c9d5c185240be4071a422aef1151
Tom John <tom.john@rea-group.com>
2016-08-05 14:26:08 +1000

    Refactor so all methods return 'IO Robot' so we can chain with '>=>' operator

--- robot.hs
+++ robot.hs
@@ -1,5 +1,7 @@
 module ToyRobot where
 
+import Control.Monad ((>=>))
+
 data Direction = North | East | South | West
 
 data Position = Position Int Int
@@ -18,26 +20,26 @@ printDirection East = "East"
 printDirection South = "South"
 printDirection West = "West"
 
-place :: Position -> Robot -> Robot
-place p (Robot d _) = Robot d p
+place :: Position -> Robot -> IO Robot
+place p (Robot d _) = return (Robot d p)
 
-turnLeft :: Robot -> Robot
-turnLeft (Robot North p) = Robot West p
-turnLeft (Robot East p) = Robot North p
-turnLeft (Robot South p) = Robot East p
-turnLeft (Robot West p) = Robot South p
+turnLeft :: Robot -> IO Robot
+turnLeft (Robot North p) = return (Robot West p)
+turnLeft (Robot East p) = return (Robot North p)
+turnLeft (Robot South p) = return (Robot East p)
+turnLeft (Robot West p) = return (Robot South p)
 
-turnRight :: Robot -> Robot
-turnRight (Robot North p) = Robot East p
-turnRight (Robot East p) = Robot South p
-turnRight (Robot South p) = Robot West p
-turnRight (Robot West p) = Robot North p
+turnRight :: Robot -> IO Robot
+turnRight (Robot North p) = return (Robot East p)
+turnRight (Robot East p) = return (Robot South p)
+turnRight (Robot South p) = return (Robot West p)
+turnRight (Robot West p) = return (Robot North p)
 
-move :: Robot -> Robot
-move (Robot North (Position x y)) = Robot North (Position x (y + 1))
-move (Robot East (Position x y)) = Robot East (Position (x + 1) y)
-move (Robot South (Position x y)) = Robot South (Position x (y - 1))
-move (Robot West (Position x y)) = Robot West (Position (x - 1) y)
+move :: Robot -> IO Robot
+move (Robot North (Position x y)) = return (Robot North (Position x (y + 1)))
+move (Robot East (Position x y)) = return (Robot East (Position (x + 1) y))
+move (Robot South (Position x y)) = return (Robot South (Position x (y - 1)))
+move (Robot West (Position x y)) = return (Robot West (Position (x - 1) y))
 
 report :: Robot -> IO Robot
 report r = do
@@ -46,5 +48,11 @@ report r = do
 
 main :: IO Robot
 main = do
-  report((move . move . turnLeft . move . turnLeft . move . move . move) r)
+  (move >=> report >=>
+   move >=> report >=>
+   place (Position 1 1) >=> report >=>
+   turnLeft >=> report >=>
+   move >=> report >=>
+   turnLeft >=> report >=>
+   move >=> report) r
   where r = Robot North (Position 2 3)
