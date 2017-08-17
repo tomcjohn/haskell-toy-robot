f43134f9d0ebe842b535c6155e8ad346fb50da09
Tom John <tom.john@rea-group.com>
2017-08-17 15:27:07 +1000

    Add Command.hs and code to read full contents of input file

---
+++ Command.hs
@@ -0,0 +1,21 @@
+module Command where
+
+import Data.List.Split
+
+import Direction
+import Position
+
+data Command = Place Position Direction | Move | Left | Right | Report | Unrecognised
+
+lookup :: String -> String -> Command
+lookup "PLACE" s = do
+  let splitCmd = splitOn " " s
+  let newX = read (splitCmd!!0)
+  let newY = read (splitCmd!!1)
+  let newDir = Direction.lookup (splitCmd!!2)
+  Place (Position newX newY) newDir
+lookup "MOVE" _ = Move
+lookup "LEFT" _ = Command.Left
+lookup "RIGHT" _ = Command.Right
+lookup "REPORT" _ = Report
+lookup _ _ = Unrecognised
--- Main.hs
+++ Main.hs
@@ -7,4 +7,8 @@ import Robot
 main :: IO ()
 main = do
   let r = Robot (Position 1 3) North
+
+  let filename = "robot-test.in"
+  _ <- readFile filename
+
   Robot.report r
