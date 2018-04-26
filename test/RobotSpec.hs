b2cee2b5b8e375761e7a882cddce52aff0f3c4e3
Tom John <tom.john@rea-group.com>
2018-04-26 20:22:12 +1000

    Fix compilation errors in test

--- test/RobotSpec.hs
+++ test/RobotSpec.hs
@@ -10,13 +10,12 @@ spec :: Spec
 spec =
   describe "move" $ do
     it "moves the robot one space relative to its direction" $ do
-      checkRobot (move $ Robot North (2,5)) North (2,6)
-      checkRobot (move $ Robot East  (2,5)) East  (3,5)
-      checkRobot (move $ Robot South (2,5)) South (2,4)
-      checkRobot (move $ Robot West  (2,5)) West  (1,5)
+      checkRobot (move $ Robot (2,5) North) North (2,6)
+      checkRobot (move $ Robot (2,5) East)  East  (3,5)
+      checkRobot (move $ Robot (2,5) South) South (2,4)
+      checkRobot (move $ Robot (2,5) West)  West  (1,5)
 
-checkRobot :: IO Robot -> Direction -> Position -> Expectation
-checkRobot ioR o p = do
-  r <- ioR
+checkRobot :: Robot -> Direction -> Position -> Expectation
+checkRobot r o p = do
   direction r `shouldBe` o
-  position    r `shouldBe` p
+  position  r `shouldBe` p
